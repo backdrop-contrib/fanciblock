7005dd4d7bafa264068c2309e0d41f7dae4250fd
docwilmot <docwilmot@gmail.com>
2015-03-06 07:27:13 -0500

    Init

---
+++ theme/fanciblock-vertical.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Default output for a fanciblock.
+*/
+?>
+<div class="fanciblock-wrapper">
+  <div class="fanciblock" id="fanciblock-<?php print $id; ?>">
+    <div class="fanciblock-grid fanciblock-grid-<?php print $grid; ?>"><div class="fanciblock-grid-inner">
+      <?php foreach($items as $item) : ?>
+        <div <?php print backdrop_attributes(array('class' => $col_class)) ?>><div class="fanciblock-col-inner">
+          <div class="fanciblock-box <?php print $box_class; ?>" <?php print $box_style; ?>>
+            <div class="fanciblock-icon"><?php print $item['image'] ?></div>
+            <div class="fanciblock-title-content">
+              <h3 class="fanciblock-title"><?php print render($item['box_title']); ?></h3>
+              <div class="fanciblock-content"><?php print render($item['box_content']); ?></div>
+            </div>
+          </div>
+        </div></div>
+      <?php endforeach; ?>
+      <div class="clear"></div>
+    </div></div>
+  </div>
+</div>
