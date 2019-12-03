e3a7c3b167e114d84cedb98795c1416430f45793
docwilmot <docwilmot@users.noreply.github.com>
2019-12-03 18:30:33 -0500

    Coding standards

--- js/fanciblock.js
+++ js/fanciblock.js
@@ -14,7 +14,7 @@ $(window).load(function(){
       // Content padding will approximate 10 as box_padding approaches 0.
       padding_corrector = parseInt(box_padding) + (10/((100*parseInt(box_padding))+1));
 
-      if($(this).find(".fanciblock-icon-size").length>0 && $(this).find(".fanciblock-icon-style-none").length==0){
+      if ($(this).find(".fanciblock-icon-size").length>0 && $(this).find(".fanciblock-icon-style-none").length==0){
       fa_icon_size = fa_icon_size+(fa_icon_size/2);
       }
 
@@ -22,15 +22,15 @@ $(window).load(function(){
       fa_icon_pos_right = $(this).find(".fanciblock-icon-position-right");
       fa_icon_pos_boxcut = $(this).find(".fanciblock-icon-position-boxcut");
 
-      if(fa_icon_pos_left.length>0){
+      if (fa_icon_pos_left.length>0){
       $(this).find( ".fanciblock-title-content" ).css({'margin-left': fa_icon_size, 'padding-left': padding_corrector});
       $(this).find(".fanciblock-icon").css({top: box_padding, left: box_padding, position:'absolute'});
       }
-      if(fa_icon_pos_right.length>0){
+      if (fa_icon_pos_right.length>0){
       $(this).find( ".fanciblock-title-content" ).css({'margin-right': fa_icon_size, 'padding-right': padding_corrector});
       $(this).find(".fanciblock-icon").css({top: box_padding, right: box_padding, position:'absolute'});
       }
-      if(fa_icon_pos_boxcut.length>0){
+      if (fa_icon_pos_boxcut.length>0){
         padding_top = parseInt($(this).find(".fanciblock-box").css('padding-top'));
       $(this).find(".fanciblock-box").css('margin-top', (fa_icon_size/2));
       $(this).find(".fanciblock-icon").css('margin-top', -(padding_top + fa_icon_size/2));
--- theme/fanciblock-vertical.tpl.php
+++ theme/fanciblock-vertical.tpl.php
@@ -7,7 +7,7 @@
 <div class="fanciblock-wrapper">
   <div class="fanciblock" id="fanciblock-<?php print $id; ?>">
     <div class="fanciblock-grid fanciblock-grid-<?php print $grid; ?>"><div class="fanciblock-grid-inner">
-      <?php foreach($items as $item) : ?>
+      <?php foreach ($items as $item) : ?>
         <div <?php print backdrop_attributes(array('class' => $col_class)) ?>><div class="fanciblock-col-inner">
           <div class="fanciblock-box <?php print $box_class; ?>" <?php print $box_style; ?>>
             <div class="fanciblock-icon"><?php print $item['image'] ?></div>
